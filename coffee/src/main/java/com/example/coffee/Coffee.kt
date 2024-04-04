5d90c9903e48884a7a8a17d2ea2d43e99e374890
Ethan <fanfirst4@naver.com>
2024-04-04 22:19:04 +0900

    Coffee

--- coffee/src/main/java/com/example/coffee/Coffee.kt
+++ coffee/src/main/java/com/example/coffee/Coffee.kt
@@ -1,2 +1,27 @@
 package com.example.coffee
 
+fun main() {
+
+    println("Who is this coffee for?")
+    val name = readln()
+
+    println("how many pieces of sugar do you want?")
+    val sugarCount = readln().toInt()
+
+    // Call Function
+    makeCoffee(sugarCount, name)
+}
+
+// Define Function
+fun makeCoffee(sugarCount: Int, name : String) {
+
+    if(sugarCount == 1) {
+        println("$name, Coffee with $sugarCount spoon of sugar")
+    } else if(sugarCount == 0) {
+        println("Coffee with no sugar for $name")
+    }
+    else {
+        println("$name, Coffee with $sugarCount spoons of sugar ")
+    }
+
+}
