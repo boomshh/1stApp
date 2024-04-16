9e5bdea958fffbb9f6922d0a4b829cdb21167218
Ethan <fanfirst4@naver.com>
2024-04-16 22:00:11 +0900

    TextField

--- unitconverter/src/main/java/com/example/composeapp/MainActivity.kt
+++ unitconverter/src/main/java/com/example/composeapp/MainActivity.kt
@@ -6,9 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,34 +34,30 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UnitConverter() {
     // 열
     Column {
         // Cloumn에는 UI 요소들이 하나씩 쌓인다.
         // 행
-        Greeting(name = "Android")
-        Greeting(name = "Android")
+        Text(text = "Unit Converter")
+        OutlinedTextField(value = " ", onValueChange = {
+            // Here goes what should happen, when the value of our outlinedTextField changes
+        })
+
         Row {
             // Here all the UI elements will be stacked next to each other
-            Greeting(name = "Android")
-            Greeting(name = "Android")
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     RockPaperScissorsTheme {
-        Greeting("Android")
+        UnitConverter()
     }
 }
