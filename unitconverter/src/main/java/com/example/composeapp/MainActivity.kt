b5ff6dda5687015bc1d18e1e2b60c8621672a541
Ethan <fanfirst4@naver.com>
2024-04-17 21:20:17 +0900

    button, spacer

--- unitconverter/src/main/java/com/example/composeapp/MainActivity.kt
+++ unitconverter/src/main/java/com/example/composeapp/MainActivity.kt
@@ -3,18 +3,29 @@ package com.example.composeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composeapp.ui.theme.RockPaperScissorsTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,26 +49,51 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun UnitConverter() {
     // 열
-    Column {
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
         // Cloumn에는 UI 요소들이 하나씩 쌓인다.
         // 행
         Text(text = "Unit Converter")
-        OutlinedTextField(value = " ", onValueChange = {
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(value = "입력하세요", onValueChange = {
             // Here goes what should happen, when the value of our outlinedTextField changes
         })
+        Spacer(modifier = Modifier.height(16.dp))
 
         Row {
+//            val context = LocalContext.current
             // Here all the UI elements will be stacked next to each other
+//            Button(onClick = { Toast.makeText(context, "Thanks for cliking",
+//                Toast.LENGTH_LONG).show() }) {
+//                Text("Click Me")
+//            }
+            Box {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")
+                }
+
+            }
+
+            Box {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "")
+                }
+
+            }
 
         }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text("Result:")
     }
 }
 
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    RockPaperScissorsTheme {
-        UnitConverter()
-    }
+fun UnitConverterPreview() {
+    UnitConverter()
 }
