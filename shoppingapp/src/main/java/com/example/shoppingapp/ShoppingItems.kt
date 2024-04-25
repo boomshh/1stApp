5c12173ea28e53afa129748cc94d84f172bf290d
Ethan <fanfirst4@naver.com>
2024-04-25 21:42:22 +0900

    dialog design

--- shoppingapp/src/main/java/com/example/shoppingapp/ShoppingItems.kt
+++ shoppingapp/src/main/java/com/example/shoppingapp/ShoppingItems.kt
@@ -2,13 +2,16 @@ package com.example.shoppingapp
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -29,6 +32,8 @@ data class ShoppingItems(val id : Int,
 fun ShoppingListApp() {
     var sItems by remember { mutableStateOf(listOf<ShoppingItems>()) }
     var showDialog by remember { mutableStateOf(false) }
+    var itemName by remember { mutableStateOf("") }
+    var itemQuantity by remember { mutableStateOf("") }
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -56,21 +61,53 @@ fun ShoppingListApp() {
         AlertDialog(
             onDismissRequest = { showDialog = false },
             confirmButton = {
-                Button(onClick = { showDialog = false }) { // 이 부분이 `confirmButton`입니다.
-                    Text("확인")
+                Row (modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween) {
+                    Button(onClick = {
+                        if(itemName.isNotBlank()) {
+                            val newItem = ShoppingItems(
+                                id = sItems.size + 1,
+                                name = itemName,
+                                quantity = itemQuantity.toInt()
+                            )
+                            sItems = sItems + newItem
+                            showDialog = false
+                            itemName = ""
+                        }
+                    }) {
+                        Text(text = "Add")
+                    }
+                    Button(onClick = { showDialog = false}) {
+                        Text(text = "Cancel")
+                    }
                 }
             },
-            text = { // `text`는 대화 상자의 본문 내용입니다.
-                Text("내용")
+            title = { // 선택적입니다. 대화 상자의 제목.
+                Text("Add Shopping Item")
             },
-            title = { // 선택적입니다. 대화 상자의 제목입니다.
-                Text("제목")
-            },
-            dismissButton = { // 선택적이지만 추가할 수 있습니다.
-                Button(onClick = { showDialog = false }) {
-                    Text("닫기")
+            text = { // `text`는 대화 상자의 본문 내용입.
+                Column {
+                    OutlinedTextField(
+                        value = itemName,
+                        onValueChange = { itemName = it},
+                        singleLine = true,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    )
+                    OutlinedTextField(
+                        value = itemQuantity,
+                        onValueChange = { itemQuantity = it},
+                        singleLine = true,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    )
                 }
-            }
+            },
+
         )
     }
 }
