4452c7f53610c23ce1363ceec43776d5fa3e75dc
Ethan <fanfirst4@naver.com>
2024-05-16 21:31:37 +0900

    주소 업데이트

---
+++ shoppingapp/src/main/java/com/example/shoppingapp/GeocodingApiService.kt
@@ -0,0 +1,13 @@
+package com.example.shoppingapp
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface GeocodingApiService {
+
+    @GET("maps/api/geocode/json")
+    suspend fun getAddressFromCoordinates(
+        @Query("latlng") latlng: String,
+        @Query("key") apikey: String
+    ) : GeocodingResponse
+}
--- shoppingapp/src/main/java/com/example/shoppingapp/LocationViewModel.kt
+++ shoppingapp/src/main/java/com/example/shoppingapp/LocationViewModel.kt
@@ -1,14 +1,34 @@
 package com.example.shoppingapp
 
+import android.util.Log
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 class LocationViewModel: ViewModel() {
     private val _location = mutableStateOf<LocationData?>(null)
     val location : State<LocationData?> = _location
 
+    private val _address = mutableStateOf(listOf<GeoCodingResult>())
+    val address : State<List<GeoCodingResult>> = _address
+
     fun updateLocation(newLocation: LocationData) {
         _location.value = newLocation
+    }
+
+    fun fetchAddress(latlng: String) {
+        try {
+            viewModelScope.launch {
+                val result = RetrofitClient.create().getAddressFromCoordinates(
+                    latlng,
+                    "AIzaSyAeyxTWH7auEUdJVKtFu9-55pp0L4Rz4vA"
+                )
+                _address.value = result.result
+            }
+        } catch(e:Exception) {
+            Log.d("res1", "${e.cause} ${e.message}")
+        }
     }
 }
