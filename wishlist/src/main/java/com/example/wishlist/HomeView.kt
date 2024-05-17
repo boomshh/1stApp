5c86c1c1c0e46127a7426e8dd4145aa1fd5a71ac
Ethan <fanfirst4@naver.com>
2024-05-17 22:55:21 +0900

    [UI] Floating Button

--- wishlist/src/main/java/com/example/wishlist/HomeView.kt
+++ wishlist/src/main/java/com/example/wishlist/HomeView.kt
@@ -1,20 +1,46 @@
 package com.example.wishlist
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeView() {
+    val context = LocalContext.current
 
     Scaffold(
-        topBar = {AppBarView(title = "WishList")}
+        topBar = {AppBarView(title = "WishList"
+        ) {
+
+            Toast.makeText(context, "Button Clicked", Toast.LENGTH_LONG).show()
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(modifier = Modifier.padding(all = 20.dp),
+                contentColor = Color.White,
+                backgroundColor = Color.Black,
+                    onClick = { // TODO add navigation to edit screen
+                    }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+
+            }
+        }
 
     ) {
-        LazyColumn(modifier = Modifier.fillMaxSize().padding(it)) {
+        LazyColumn(modifier = Modifier
+            .fillMaxSize()
+            .padding(it)) {
 
         }
 
