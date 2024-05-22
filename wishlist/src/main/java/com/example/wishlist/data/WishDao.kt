3840e0d295e1dfd8afcdc43b2ebbec5466e0f8b1
Ethan <fanfirst4@naver.com>
2024-05-22 22:51:09 +0900

    DAO 설정

---
+++ wishlist/src/main/java/com/example/wishlist/data/WishDao.kt
@@ -0,0 +1,29 @@
+package com.example.wishlist.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+abstract class WishDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    abstract suspend fun addWish(wishEntity: Wish)
+
+    // wish-table에 있는 모든 wish 항목을 load로 가져옴
+    @Query("Select * from `wish-table`")
+    abstract fun getAllWishes(): Flow<List<Wish>>
+
+    @Update
+    abstract suspend fun updateWish(wishEntity: Wish)
+
+    @Delete
+    abstract suspend fun deleteWish(wishEntity: Wish)
+
+    @Query("Select * from `wish-table` where id =:id")
+    abstract fun getWishById(id: Long) : Flow<Wish>
+}
