470ce062fbe8503348870a951e51869518ed0bd6
Ethan <fanfirst4@naver.com>
2024-05-23 21:38:24 +0900

    database 설정

---
+++ wishlist/src/main/java/com/example/wishlist/Graph.kt
@@ -0,0 +1,17 @@
+package com.example.wishlist
+
+import android.content.Context
+import androidx.room.Room
+import com.example.wishlist.data.WishDatabase
+
+object Graph {
+    lateinit var database: WishDatabase
+
+    val wishRepository by lazy {
+        WishRepository(wishDao = database.wishDao())
+    }
+
+    fun provide(context: Context) {
+        database = Room.databaseBuilder(context, WishDatabase::class.java, "wishlist.db").build()
+    }
+}
