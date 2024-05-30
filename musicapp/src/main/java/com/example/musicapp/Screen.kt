309bb62451254f1eec172ecded6fb17ebb071b83
Ethan <fanfirst4@naver.com>
2024-05-30 22:50:20 +0900

    topbar menu 만들기

---
+++ musicapp/src/main/java/com/example/musicapp/Screen.kt
@@ -0,0 +1,33 @@
+package com.example.musicapp
+
+import androidx.annotation.DrawableRes
+
+sealed class Screen(val title: String, val route: String) {
+
+    sealed class DrawerScreen(val dTitle: String, val dRoute: String, @DrawableRes val icon: Int)
+        :Screen(dTitle, dRoute) {
+            object Account: DrawerScreen(
+                "Account",
+                "account",
+                R.drawable.baseline_account_box_24
+            )
+
+        object Subscription: DrawerScreen(
+            "Subscription",
+            "subscribe",
+            R.drawable.baseline_library_music_24
+        )
+
+        object AddAccount: DrawerScreen(
+            "Add Account",
+            "add_account",
+            R.drawable.baseline_person_add_alt_1_24
+        )
+        }
+}
+
+val screensInDrawer = listOf(
+    Screen.DrawerScreen.Account,
+    Screen.DrawerScreen.Subscription,
+    Screen.DrawerScreen.AddAccount
+)
